e8fcd2a6e17707ae19b4a36e17170c8f35f4b438
ArtiGaund <artigaund2210@gmail.com>
2020-09-28 11:39:39 +0530

    testing

---
+++ projfrontend/src/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './core/Home';
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import Cart from './core/Cart';
+import PrivateRoutes from "./auth/helper/PrivateRouters";
+import UserDashboard from "./user/UserDashBoard";
+const Routes = () =>{
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home}/>
+                <Route path="/signup" exact component={Signup}/>
+                <Route path="/signin" exact component={Signin}/>
+                <Route path="/cart" exact component={Cart}/>
+                <PrivateRoutes path="/user/dashboard" exact component={UserDashboard} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
